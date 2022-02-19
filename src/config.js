9049d9695bbc617a6637e3106f30090dfa1d8357
unknown <jouiwnl>
2022-02-19 20:40:37 -0300

    muda url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    //baseURL: 'http://localhost:8080/'
-    baseURL: 'http://ec2-54-94-165-247.sa-east-1.compute.amazonaws.com:8080/'
+    baseURL: 'http://localhost:8080/'
+    //baseURL: 'https://ec2-54-94-165-247.sa-east-1.compute.amazonaws.com/'
 });
--- src/modules/partners/services/PartnerService.js
+++ src/modules/partners/services/PartnerService.js
@@ -5,15 +5,15 @@ export default {
         return http.get(`agencias`)
     },
     findById: (agenciaId) => {
-        return http.get(`agencias/${agenciaId}`)
+        return http.get(`agencias/${agenciaId}/`)
     },
     create: (agencia) => {
-        return http.post('agencias', agencia);
+        return http.post('agencias/', agencia);
     },
     inactive: (agenciaId) => {
-        return http.patch(`agencias/${agenciaId}/inativar`)
+        return http.patch(`agencias/${agenciaId}/inativar/`)
     },
     update: (agencia, agenciaId) => {
-        return http.put(`agencias/${agenciaId}`, agencia)
+        return http.put(`agencias/${agenciaId}/`, agencia)
     }
 }
